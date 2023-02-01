3c4a8de1a349a86b09977a43976a7553aef2c2fa
Eskual <panichukd@gmail.com>
2023-02-01 23:32:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 setup(name='clean-folder',
       version='1',
       description='Script for organizing your "download" or "trash" folders',
-      url='http://github.com/dummy_user/useful',
+      url='https://github.com/Eskual/mod-6',
       author='Eskual',
       author_email='panichukd@gmail.com',
       license='MIT',
